f9b7aef6fb9c5f3a6c86526aee437e24326b77ea
Your Name <f.omarfaruk01@gmail.com>
2023-07-03 16:45:01 +0600

    First commit

---
+++ android/app/src/main/kotlin/com/example/flutter_256_videos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_256_videos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text("Its my first day of learning flutter , alhamdulillah"),
+        ),
+      ),
+    );
+  }
+}
+
+
+
